d379bb3664e1b12583abbf364db62d727d014cf2
Osunde Goodluck Michael <michaelking@Osundes-MacBook-Pro.local>
2025-04-26 11:19:18 +0100

    udpate

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards, Res } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Res, Get, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dtos/signup.dto';
 import { LoginDto } from './dtos/login.dto';
@@ -6,6 +6,7 @@ import { RefreshTokenDto } from './dtos/refresh.token.dto';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/common/guards/jwt.guard';
 import { Response } from 'express';
+import { User } from 'src/common/decorators/user.decorator';
 
 
 @ApiTags('auth')
@@ -13,11 +14,11 @@ import { Response } from 'express';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-//  to be deleted
-  // @Post('signup')
-  // signup(@Body() dto: SignupDto) {
-  //   return this.authService.signup(dto);
-  // }
+
+  
+  
+  
+  
 
  
   @ApiOperation({summary: "Login"})
@@ -25,22 +26,22 @@ export class AuthController {
   @ApiResponse({ status: 401, description: 'Unauthorized.' })
   @ApiResponse({ status: 500, description: 'Internal Server Error.' })
   @Post('login')
-async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: Response) {
-  const loginResult = await this.authService.login(dto);
+  async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: Response) {
+    const loginResult = await this.authService.login(dto);
 
-  res.cookie('access_token', loginResult.accessToken, {
-    httpOnly: true,  // Cannot be accessed by JavaScript (good security)
-    secure: false,   // true if using https
-    sameSite: 'lax', // protection against CSRF
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
-  });
+    res.cookie('access_token', loginResult.accessToken, {
+      httpOnly: true,  
+      secure: false,   
+      sameSite: 'lax', 
+      maxAge: 24 * 60 * 60 * 1000, 
+    });
 
-  // You can still return user data if you want
-  return {
-    message: "Login successful",
-    user: loginResult.user,
-  };
-}
+    
+    return {
+      message: "Login successful",
+      
+    };
+  }
 
   
   @ApiBearerAuth('access-token')
@@ -51,5 +52,10 @@ async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: Response) {
   @ApiResponse({ status: 500, description: 'Internal Server Error.' })
   async refreshTokens(@Body() dto: RefreshTokenDto) {
     return this.authService.refreshTokens(dto.refreshToken);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@User() user: any) {
+    return user;  
   }
 }
